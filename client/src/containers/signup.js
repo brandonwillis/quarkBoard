d32a0fda63248308fe3197dc07405bfe10339174
Brandon Willis <brandonlynnwillis@gmail.com>
2016-06-08 20:34:16 -0700

    [feat] adds updated signup page

--- client/src/containers/signup.js
+++ client/src/containers/signup.js
@@ -10,15 +10,15 @@ class SignUp extends Component {
 
   onSubmit(props) {
     console.log("Sign Up props: ", props)
-    this.props.signIn(props);
+    this.props.signUp(props);
   }
 
   render() {
-    const { fields: { username, password }, handleSubmit } = this.props
+    const { fields: { username, email, password, confirmPassword}, handleSubmit } = this.props
 
     return (
       <form onSubmit={this.onSubmit.bind(this)}>
-        <h3>Sign In</h3>
+        <h3>Sign Up</h3>
         <div className={`form-group ${username.touched && username.invalid ? "has-danger" : '' }`}>
           <label>Username</label>
           <input type="text" className="form-control" {...username} />
@@ -26,11 +26,25 @@ class SignUp extends Component {
             {username.touched ? username.error : ""}
           </div>
         </div>
+        <div className={`form-group ${email.touched && email.invalid ? "has-danger" : '' }`}>
+          <label>Email Address</label>
+          <input type="text" className="form-control" {...email} />
+          <div className="text-help">
+            {email.touched ? email.error : ""}
+          </div>
+        </div>
         <div className={`form-group ${password.touched && password.invalid ? "has-danger" : '' }`}>
           <label>Password</label>
-          <input type="pasword" className="form-control" {...password} />
+          <input type="password" className="form-control" {...password} />
           <div className="text-help">
             {password.touched ? password.error : ""}
+          </div>
+        </div>
+        <div className={`form-group ${confirmPassword.touched && confirmPassword.invalid ? "has-danger" : '' }`}>
+          <label>Confirm Password</label>
+          <input type="password" className="form-control" {...confirmPassword} />
+          <div className="text-help">
+            {confirmPassword.touched ? confirmPassword.error : ""}
           </div>
         </div>
         <button type="submit" className="btn btn-primary">Sign In</button>
@@ -46,8 +60,17 @@ function validate(values) {
     errors.username = "Enter a Username"
   }
 
+  if(!values.email) {
+    errors.email = "Enter a valid email address"
+  }
   if(!values.password) {
     errors.password = "Enter a password"
+  }
+  if(!values.confirmPassword) {
+    errors.confirmPassword = "Reenter your password"
+  }
+  if(values.password !== values.confirmPassword) {
+    errors.confirmPassword = "Your passwords do not match"
   }
 
   return errors;
@@ -55,6 +78,6 @@ function validate(values) {
 
 export default reduxForm({
   form: 'SignUpForm',
-  fields: ['username','password'],
+  fields: ['username', 'email', 'password', 'confirmPassword'],
   validate
 }, null, { signUp })(SignUp)
